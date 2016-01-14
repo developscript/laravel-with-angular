3b4d8827132cec8a5548312c881b19e06f94eefd
Rafael Henrique Berro <rhberro@gmail.com>
2016-01-14 11:19:58 -0200

    Form field validation messages for login and register page.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,25 +6,28 @@
 
     <md-input-container class="md-block">
         <label>Email</label>
-        <input type="text" name="email" ng-model="credentials.email" required/>
+
+        <input type="email" name="email" ng-model="credentials.email" required/>
+
         <div ng-messages="loginForm.email.$error">
-            <div ng-message="required">
-                Required.
-            </div>
-            <div ng-message="pattern">
-                Email.
+            <div ng-message-exp="['required', 'email']">
+                Your email is required and must look like an e-mail address.
             </div>
         </div>
+
     </md-input-container>
 
     <md-input-container class="md-block">
         <label>Password</label>
-        <input type="password" name="password" ng-model="credentials.password" required/>
+
+        <input type="password" name="password" ng-model="credentials.password" minlength="6" maxlength="16" required/>
+
         <div ng-messages="loginForm.password.$error">
-            <div ng-message="required">
-                Required.
+            <div ng-message-exp="['required', 'minlength', 'maxlength']">
+                Your password is required and must be between 6 and 16 characters long.
             </div>
         </div>
+
     </md-input-container>
 
     <md-input-container class="md-block">
@@ -40,3 +43,9 @@
     </md-input-container>
 
 </form>
+
+<div ng-if="errors">
+    <p ng-repeat="error in errors">
+        {{ error }}
+    </p>
+</div>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,45 +6,50 @@
 
     <md-input-container class="md-block">
         <label>Name</label>
-        <input type="text" name="name" ng-model="credentials.name" required/>
+
+        <input type="text" name="name" ng-model="credentials.name" minlength="3" required/>
+
         <div ng-messages="registerForm.name.$error">
-            <div ng-message="required">
-                Required.
+            <div ng-message-exp="['required', 'minlength']">
+                Your name is required and must have at least 3 characters long.
             </div>
         </div>
     </md-input-container>
 
     <md-input-container class="md-block">
         <label>Email</label>
+
         <input type="email" name="email" ng-model="credentials.email" required/>
+
         <div ng-messages="registerForm.email.$error">
-            <div ng-message="required">
-                Required.
-            </div>
-            <div ng-message="email">
-                Email.
+            <div ng-message-exp="['required', 'email']">
+                Your email is required and must look like an e-mail address.
             </div>
         </div>
     </md-input-container>
 
     <md-input-container class="md-block">
         <label>Password</label>
-        <input type="password" name="password" ng-model="credentials.password" required/>
+
+        <input type="password" name="password" ng-model="credentials.password" minlength="6" maxlength="16" required/>
+
         <div ng-messages="registerForm.password.$error">
-            <div ng-message="required">
-                Required.
+            <div ng-message-exp="['required', 'minlength', 'maxlength']">
+                Your password is required and must be between 6 and 16 characters long.
             </div>
         </div>
     </md-input-container>
 
     <md-input-container class="md-block">
         <label>Repeat Password</label>
-        <input type="password" name="password_confirmation" ng-model="credentials.password_confirmation" required/>
+
+        <input type="password" name="password_confirmation" ng-model="credentials.password_confirmation" minlength="6" maxlength="16" required/>
+
         <div ng-messages="registerForm.password_confirmation.$error">
-            <div ng-message="required">
-                Required.
+            <div ng-message-exp="['required', 'minlength', 'maxlength']">
+                Your password is required and must be between 6 and 16 characters long.
             </div>
-        </div>
+        </div>  
     </md-input-container>
 
     <md-input-container class="md-block">
@@ -60,3 +65,9 @@
     </md-input-container>
 
 </form>
+
+<div ng-if="errors">
+    <p ng-repeat="error in errors">
+        {{ error }}
+    </p>
+</div>
