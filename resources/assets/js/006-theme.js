ebd1511054a01d73fc4cbfc482698e5f3d96eb76
Rafael Henrique Berro <rhberro@gmail.com>
2016-01-14 22:42:22 -0200

    Added theming provider.

---
+++ resources/assets/js/006-theme.js
@@ -0,0 +1,9 @@
+app
+.config(function ($mdThemingProvider) {
+	/*
+	 * See https://material.angularjs.org/latest/Theming/03_configuring_a_theme for more info.
+	 */
+	$mdThemingProvider
+		.theme('default')
+		.primaryPalette('red');
+});
